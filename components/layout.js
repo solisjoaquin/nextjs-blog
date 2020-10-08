d49a6c1d1a89c95603ff7335a33cf4e71d4baf4a
Joaquin <sol19008@byui.edu>
2020-10-08 17:01:04 -0300

    changes in index.js

--- components/layout.js
+++ components/layout.js
@@ -23,6 +23,8 @@ export default function Layout({ children, home }) {
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+
+
       </Head>
       <header className={styles.header}>
         {home ? (
